bb965c9037b5167a7811f43db63d081d844cb974
laytonmusyoki <laytonmatheka@gmail.com>
2026-02-17 20:13:22 +0300

    messages

--- resources/views/web/app.blade.php
+++ resources/views/web/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-        @yield('title','Genyra Group - Healthcare Solutions Across East Africa')
+        @yield('title','genyra Group - Building Solutions | Transforming Healthcare ')
     </title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.jpeg') }}">
